426bf9ca6b2383deb759c678f7ae464fb25a4f99
shivangi-jain <shivangi.jain@sjsu.edu>
2019-11-28 00:06:34 -0600

    Minor changes

--- yoga-pose-detection/src/index.js
+++ yoga-pose-detection/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
